8c5ce2b2471b37c19dd0ed73a1fd3cd2740b1bce
landon rabern <landon.rabern@gmail.com>
2020-02-29 17:29:02 -0800

    starting to play with Hutter's compression problrm

---
+++ Hutter/Compress/Arithmetic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compress
+{
+    public static class Arithmetic
+    {
+        //public static List<int> LongestArithmeticSubsequence(IList<int> a)
+        //{
+        //    return LongestArithmeticSubsequence(a, 1);
+
+
+        //}
+
+        public static int LongestArithmeticSubsequence(IList<int> a, int step)
+        {
+            var n = a.Count;
+            var m = new Dictionary<int, int>();
+            int maxt = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (m.ContainsKey(a[i] - i * step))
+                {
+                    int freq = m[a[i] - i * step];
+                    freq++;
+                    m.Remove(a[i] - i * step);
+                    m.Add(a[i] - i * step, freq);
+                }
+                else
+                {
+                    m.Add(a[i] - i * step, 1);
+                }
+            }
+
+            foreach (var val in m)
+            {
+                if (maxt < val.Value)
+                    maxt = val.Value;
+            }
+            return maxt;
+        }
+    }
+}
---
+++ Hutter/Compress/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Compress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var counts = CharCounts();
+            //var inorder = counts.AsEnumerable().OrderBy(kv => kv.Value).Select(kv => kv.Key).ToList();
+
+            //Console.WriteLine(string.Join(',', counts.Select(kv => $"{kv.Key},{kv.Value}")));
+
+            // var indices = Indices();
+
+            var list = new List<int>(new int[] { 1, 2, 3, 7, 8, 9 });
+
+            var vv = Arithmetic.LongestArithmeticSubsequence(list, 1); ;
+
+
+        }
+
+        static Dictionary<char, List<int>> Indices()
+        {
+            var indices = new Dictionary<char, List<int>>();
+            using (var sr = new StreamReader("C:/hutter/t1.txt"))
+            {
+                var index = 0;
+                while (true)
+                {
+                    var buf = new char[1];
+                    var numReturned = sr.Read(buf, 0, 1);
+                    if (numReturned == 0)
+                        break;
+                   
+                    var c = buf[0];
+                    if (!indices.ContainsKey(c))
+                        indices[c] = new List<int>();
+                    indices[c].Add(index);
+
+                    index++;
+                }
+            }
+            return indices;
+        }
+
+        static Dictionary<char, int> CharCounts() 
+        {
+            var counts = new Dictionary<char, int>();
+            using (var sr = new StreamReader("C:/hutter/t1.txt"))
+            {
+                var index = 0;
+                while (true)
+                {
+                    var buf = new char[1];
+                    var numReturned = sr.Read(buf, 0, 1);
+                    if (numReturned == 0)
+                        break;
+                    index++;
+                    var c = buf[0];
+                    if (!counts.ContainsKey(c))
+                        counts[c] = 0;
+                    counts[c]++;
+                }
+            }
+
+            return counts;
+        }
+    }
+}
